17f7e1170f47f221588c79d754ccd49667ddd813
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-10-14 10:03:07 +0600

    addminServiceList done

--- src/Components/AdminPanel/AdminPanelForm/AdminServiceListDetail.js
+++ src/Components/AdminPanel/AdminPanelForm/AdminServiceListDetail.js
@@ -3,11 +3,45 @@ import './AdminServiceListDetail.css';
 
 const AdminServiceListDetail = () => {
     return (
-        <section className="serviceList-container">
+        <section>
             <div>
-                <h1>detaile</h1>
-            </div>
-            <div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h5>Order</h5>
+                    </div>
+                    <div className="col-md-5 ml-5">
+                        <small><h6>Pro Rasel</h6></small>
+                    </div>
+                </div>
+                <div className="order-from-body pt-3">
+
+                    <div className="serviceList-container ">
+                        <div className="row body-part-1">
+                            <div className="col-md-2 mr-3">Name</div>
+                            <div className="col-md-3 mr-4">Email ID</div>
+                            <div className="col-md-2 mr-2">Service</div>
+                            <div className="col-md-2 mr-2"> Project Details</div>
+                            <div className="col-md-2 ml-2"> Status</div>
+
+                        </div>
+                        <div className="row mt-3 body-part-2">
+                            <div className="col-md-2 mr-3">Rashidul</div>
+                            <div className="col-md-3 mr-4">rashidul.191cse.gub@gmail.com</div>
+                            <div className="col-md-2 mr-2">Graphic Design</div>
+                            <div className="col-md-2 mr-2"> Lorem ipsum dolor sit amet consectetur adipisicing elit. </div>
+                            <div className="col-md-2 ml-2">
+                                <div class="form-group">
+                                    <select class="form-control option-style" >
+                                        <option className="text-option1">Pending</option>
+                                        <option className="text-option2">Done</option>
+                                        
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
         </section>
